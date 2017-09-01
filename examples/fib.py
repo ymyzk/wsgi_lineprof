e323b3deef0c7fa9136f1836ac0eea769d48bdcb
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2017-09-01 18:21:34 +0900

    Add examples/fib.py

---
+++ examples/fib.py
@@ -0,0 +1,39 @@
+from wsgiref.util import setup_testing_defaults
+from wsgiref.simple_server import make_server
+
+from wsgi_lineprof.filters import FilenameFilter, TotalTimeSorter
+from wsgi_lineprof.middleware import LineProfilerMiddleware
+
+
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+
+# Simple WSGI application
+def app(environ, start_response):
+    setup_testing_defaults(environ)
+
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain; charset=utf-8')]
+
+    start_response(status, headers)
+
+    n = 30
+    fib_n = fib(n)
+    res = "fib(%d) = %d" % (n, fib_n)
+    return [res.encode("utf-8")]
+
+
+# Set up profiler
+filters = [
+    FilenameFilter("fib.py"),
+    TotalTimeSorter(),
+]
+app = LineProfilerMiddleware(app, filters=filters)
+
+
+with make_server('127.0.0.1', 8000, app) as httpd:
+    print("Serving on 127.0.0.1:8000...")
+    httpd.serve_forever()
