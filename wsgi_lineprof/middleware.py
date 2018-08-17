c346c9f262270916f4c6875e8d2903b3796344c7
denzow <denzow@gmail.com>
2018-08-17 23:52:39 +0900

    Add buffer size option for writing stats.

--- wsgi_lineprof/middleware.py
+++ wsgi_lineprof/middleware.py
@@ -10,12 +10,19 @@ class LineProfilerMiddleware(object):
     def __init__(self,
                  app,  # type: Callable
                  stream=None,  # type: Union[TextIO, TextIOWrapper]
-                 filters=tuple()  # type: Iterable[FilterType]
+                 filters=tuple(),  # type: Iterable[FilterType]
+                 buffer_size=0  # type: int
                  ):
         # type: (...) -> None
         self.app = app
         self.stream = sys.stdout if stream is None else stream
         self.filters = filters
+        self.buffer_size = buffer_size
+        self._stats_buffer = []
+        self.write_stats = self._write_stats
+        # if buffer enable, switch write stats function
+        if self.buffer_size > 0:
+            self.write_stats = self._write_stats_with_buffer
 
     def __call__(self, env, start_response):
         profiler = LineProfiler()
@@ -28,7 +35,15 @@ class LineProfilerMiddleware(object):
 
         for f in self.filters:
             stats = stats.filter(f)
-
-        stats.write_text(self.stream)
+        self.write_stats(stats)
 
         return result
+
+    def _write_stats(self, stats):
+        stats.write_text(self.stream)
+
+    def _write_stats_with_buffer(self, stats):
+        self._stats_buffer.append(stats)
+        if len(self._stats_buffer) > self.buffer_size:
+            [s.write_text(self.stream) for s in self._stats_buffer]
+            self._stats_buffer = []
