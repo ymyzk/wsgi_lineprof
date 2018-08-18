8425896bb0bf36f0712c333b37f09be061e67d61
denzow <denzow@gmail.com>
2018-08-18 13:27:15 +0900

    Writer thread impl

--- wsgi_lineprof/middleware.py
+++ wsgi_lineprof/middleware.py
@@ -1,21 +1,36 @@
 from io import TextIOWrapper  # noqa: F401
 import sys
+from queue import Queue, Empty
+from threading import Thread
 from typing import Callable, Iterable, TextIO, Union  # noqa: F401
 
 from wsgi_lineprof.profiler import LineProfiler
 from wsgi_lineprof.stats import FilterType  # noqa: F401
 
 
+def writer(stats_queue, stream):
+    while True:
+        try:
+            stats = stats_queue.get(timeout=5)
+            stats.write_text(stream)
+        except Empty:
+            continue
+
+
 class LineProfilerMiddleware(object):
     def __init__(self,
                  app,  # type: Callable
                  stream=None,  # type: Union[TextIO, TextIOWrapper]
-                 filters=tuple()  # type: Iterable[FilterType]
+                 filters=tuple(),  # type: Iterable[FilterType]
                  ):
         # type: (...) -> None
         self.app = app
         self.stream = sys.stdout if stream is None else stream
         self.filters = filters
+        self.queue = Queue()  # type: Queue
+        self.writer_thread = Thread(target=writer, args=(self.queue, self.stream))
+        self.writer_thread.setDaemon(True)
+        self.writer_thread.start()
 
     def __call__(self, env, start_response):
         profiler = LineProfiler()
@@ -29,6 +44,6 @@ class LineProfilerMiddleware(object):
         for f in self.filters:
             stats = stats.filter(f)
 
-        stats.write_text(self.stream)
+        self.queue.put(stats)
 
         return result
