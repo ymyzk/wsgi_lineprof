6ac2c899a2d73844483df9bd545eb33283948f3d
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2018-08-25 21:29:37 +0900

    Suppress type errors

--- wsgi_lineprof/middleware.py
+++ wsgi_lineprof/middleware.py
@@ -18,10 +18,11 @@ class LineProfilerMiddleware(object):
         self.app = app
         self.stream = sys.stdout if stream is None else stream
         self.filters = filters
+        # TODO: "type: ignore" is for supressing mypy errors
         if async_stream:
-            self.writer = AsyncWriter(self.stream)  # type: Any
+            self.writer = AsyncWriter(self.stream)  # type: ignore
         else:
-            self.writer = SyncWriter(self.stream)
+            self.writer = SyncWriter(self.stream)  # type: ignore
 
     def __call__(self, env, start_response):
         profiler = LineProfiler()
