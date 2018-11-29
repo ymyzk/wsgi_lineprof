b49b9adfb02243bceb5984598270f6b551fd4f73
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2018-11-30 01:30:55 +0900

    Use latest manylinux1 Docker image for building wheel

--- scripts/build_manylinux1_wrapper.sh
+++ scripts/build_manylinux1_wrapper.sh
@@ -4,5 +4,7 @@ set -e
 cd $(dirname $0)
 cd ..
 
+docker pull quay.io/pypa/manylinux1_x86_64
+docker pull quay.io/pypa/manylinux1_i686
 docker run --rm -v `pwd`:/app quay.io/pypa/manylinux1_x86_64 /app/scripts/build_manylinux1.sh
 docker run --rm -v `pwd`:/app quay.io/pypa/manylinux1_i686 linux32 /app/scripts/build_manylinux1.sh
