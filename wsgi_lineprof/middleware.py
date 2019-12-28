d335a008615c945492b05114334bf347e790355c
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2019-12-29 01:01:39 +0900

    Fix import errors on Python 2

--- wsgi_lineprof/middleware.py
+++ wsgi_lineprof/middleware.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import atexit
 from six.moves import reduce
 import sys
--- wsgi_lineprof/stats.py
+++ wsgi_lineprof/stats.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from types import CodeType
 from typing import Callable, Dict, Iterable, Union
 
