eb37c11ec87d3625708b6dd056563e5f6ff25da0
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2022-11-02 23:01:37 +0900

    Remove cythonize from setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from Cython.Build import cythonize
 from setuptools import Extension, setup
 
 
 setup(
     ext_package="wsgi_lineprof",
-    ext_modules=cythonize([
-        Extension("extensions",
-                  sources=["extensions/extensions.pyx", "extensions/timer.c"])
-    ]),
+    ext_modules=[
+        Extension(
+            # setuptools should discover Cython automatically
+            "extensions", sources=["extensions/extensions.pyx", "extensions/timer.c"]
+        )
+    ],
 )
