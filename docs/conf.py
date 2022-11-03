01298bbd750aadf5c7f9f1a4915ce31ee7bb0194
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2022-11-03 10:32:55 +0900

    Bump version to 0.15.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2016, Yusuke Miyazaki"
 author = "Yusuke Miyazaki"
 
 # The full version, including alpha/beta/rc tags
-release = "0.14.0"
+release = "0.15.0"
 
 
 # -- General configuration ---------------------------------------------------
